5ecd97255f5579efd7aca55542ca2717a1935cdb
rdevil23 <rdevil1337@gmail.com>
2023-03-12 23:32:26 +0500

    style: поправить cемантику/стили/фикс.ширины

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,6 @@ let popupEditForm = popupContainer.querySelector('.popup__edit-form');
 
 const editBtn = document.querySelector('.profile__edit-btn');
 const closeBtn = document.querySelector('.popup__close-btn');
-const saveBtn = document.querySelector('.popup__save-btn');
 
 let inputName = popupEditForm.querySelector('.popup__input_type_name');
 let inputJob = popupEditForm.querySelector('.popup__input_type_job');
@@ -18,25 +17,22 @@ const openPopup = function () {
   inputJob.value = profileJob.textContent;
 };
 
-const closePopup = function () {
+const closePopup = function (e) {
+  console.log(e.target, e.currentTarget);
+  if (e.target !== e.currentTarget) {
+    return;
+  }
   popup.classList.remove('popup_opened');
 };
-
-const closePopupByClickOverlay = function (e) {
-  if (e.target === e.currentTarget) {
-    closePopup();
-  }
-};
-
-editBtn.addEventListener('click', openPopup);
-closeBtn.addEventListener('click', closePopup);
-popup.addEventListener('click', closePopupByClickOverlay);
 
 function handleFormSubmit(e) {
   e.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
+  closePopup(e);
 }
 
+editBtn.addEventListener('click', openPopup);
+closeBtn.addEventListener('click', closePopup);
+popup.addEventListener('click', closePopup);
 popupEditForm.addEventListener('submit', handleFormSubmit);
-saveBtn.addEventListener('click', closePopup);
